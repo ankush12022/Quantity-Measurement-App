0eb63ef78472206ddceb2a5c22ed73371a817354
Ankush <ankushdhiman0421@gmail.com>
2026-03-18 10:33:41 +0530

    UC2 Commit

---
+++ src/main/java/org/example/uc2/QuantityMeasurementApp.java
@@ -0,0 +1,69 @@
+package org.example.uc2;
+public class QuantityMeasurementApp {
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            Feet temp = (Feet) obj;
+            return Double.compare(this.value, temp.value) == 0;
+        }
+    }
+
+    public static class Inch {
+        private final double value;
+
+        public Inch(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            Inch temp = (Inch) obj;
+            return Double.compare(this.value, temp.value) == 0;
+        }
+    }
+
+    public static void feetEquality() {
+        double val1 = 6.7;
+        double val2 = 6.7;
+
+        Feet obj1 = new Feet(val1);
+        Feet obj2 = new Feet(val2);
+
+        if (obj1.equals(obj2))
+            System.out.println("Both are equal in feet");
+        else
+            System.out.println("Values are different");
+    }
+
+    public static void inchEquality() {
+        double val1 = 10.0;
+        double val2 = 10.0;
+
+        Inch obj1 = new Inch(val1);
+        Inch obj2 = new Inch(val2);
+
+        if (obj1.equals(obj2))
+            System.out.println("Both are equal in inches");
+        else
+            System.out.println("Values are different");
+    }
+
+    public static void main(String[] args) {
+        feetEquality();
+        inchEquality();
+    }
+}
